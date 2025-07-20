e2d0b83ff34653c7ebaca954c63c8708169cc230
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2025-07-20 16:58:00 +0600

    Update sale_order_line.py

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -3,3 +3,16 @@ from odoo import models, fields, api
 class SalesOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
+
+    warehouse_lines = fields.One2many('sale.order.line.warehouse', 'order_line_id', string='Warehouses')
+
+    warehouse_name = fields.Selection(
+        string="Warehouse",
+        selection=lambda self: self._get_warehouse_selection(),
+        required=True
+    )
+
+    @api.model
+    def _get_warehouse_selection(self):
+        warehouses = self.env['stock.warehouse'].search([])
+        return [(w.name, w.name) for w in warehouses]
